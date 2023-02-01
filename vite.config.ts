5340172775d93d49f333d11faae7b5c0b7bc5e71
snerks <martin.github@outlook.com>
2023-02-01 15:19:27 +0000

    Add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/football-stats-api3/",
 
   build: {
     sourcemap: true,
